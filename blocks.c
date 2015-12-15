3d6b0d96f240a1e65d3fa2dd15edbeef62029601
August Falk <augfal@kth.se>
2015-12-15 18:41:27 +0100

    blocks fo realz

--- blocks.c
+++ blocks.c
@@ -1,41 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
 unsigned char simpleStick[4][4][2] = {{{2,31},{3,31},{4,31},{5,31}},
                                         {{3,31},{3,30},{3,29},{3,28}},
                                         {{2,31},{3,31},{4,31},{5,31}},
                                         {{3,31},{3,30},{3,29},{3,28}}
                                         };
 unsigned char simpleBox[4][4][2] = {{{3,31},{3,30},{4,31},{4,30}},
-                                        {{3,31},{3,30},{4,31},{4,30},
-                                        {{3,31},{3,30},{4,31},{4,30},
-                                        {{3,31},{3,30},{4,31},{4,30}
+                                        {{3,31},{3,30},{4,31},{4,30}},
+                                        {{3,31},{3,30},{4,31},{4,30}},
+                                        {{3,31},{3,30},{4,31},{4,30}}
                                         };
 unsigned char simplePodium[4][4][2] = {{{2,30},{3,30},{3,31},{4,30}},
-                                        {{3,31},{3,30},{3,29},{4,30},
-                                        {{2,30},{3,30},{4,30},{3,29},
-                                        {{2,30},{3,31},{3,30},{3,29}
+                                        {{3,31},{3,30},{3,29},{4,30}},
+                                        {{2,30},{3,30},{4,30},{3,29}},
+                                        {{2,30},{3,31},{3,30},{3,29}}
                                         };
 unsigned char simpleLZig[4][4][2] = {{{2,30},{3,30},{3,31},{4,31}},
-                                        {{2,31},{2,30},{3,30},{3,29},
-                                        {{2,30},{3,30},{3,31},{4,31},
-                                        {{2,31},{2,30},{3,30},{3,29}
+                                        {{2,31},{2,30},{3,30},{3,29}},
+                                        {{2,30},{3,30},{3,31},{4,31}},
+                                        {{2,31},{2,30},{3,30},{3,29}}
                                         };
 unsigned char simpleRZig[4][4][2] = {{{2,31},{3,31},{3,30},{4,30}},
-                                        {{2,30},{2,29},{3,31},{3,30},
-                                        {{2,31},{3,31},{3,30},{4,30},
-                                        {{2,30},{2,29},{3,31},{3,30}
+                                        {{2,30},{2,29},{3,31},{3,30}},
+                                        {{2,31},{3,31},{3,30},{4,30}},
+                                        {{2,30},{2,29},{3,31},{3,30}}
                                         };
 unsigned char simpleLfoot[4][4][2] = {{{2,29},{3,31},{3,30},{3,29}},
-                                        {{2,31},{2,30},{3,30},{4,30},
-                                        {{2,29},{2,30},{2,31},{3,31},
-                                        {{2,31},{3,31},{4,31},{4,30}
+                                        {{2,31},{2,30},{3,30},{4,30}},
+                                        {{2,29},{2,30},{2,31},{3,31}},
+                                        {{2,31},{3,31},{4,31},{4,30}}
                                         };
 unsigned char simpleRfoot[4][4][2] = {{{2,31},{2,30},{2,29},{3,29}},
-                                        {{2,31},{2,30},{3,31},{4,31},
-                                        {{2,31},{3,31},{3,30},{3,29},
-                                        {{2,30},{3,30},{4,30},{4,31}
+                                        {{2,31},{2,30},{3,31},{4,31}},
+                                        {{2,31},{3,31},{3,30},{3,29}},
+                                        {{2,30},{3,30},{4,30},{4,31}}
                                         };
 
 unsigned char background[8][32] = {
     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+};
+unsigned char matrix[8][32] = {
     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
@@ -51,38 +63,43 @@ unsigned char currentBlock[4][4][2];
 unsigned char orientation = 0;
 
 void newBlock(){
-    background = addBlock(background,currentBlock);
-    currentBlock = 
+    memcpy(background, matrix, 8*32);
+    memcpy(currentBlock, simpleBox, 4*4*2);//random block istället för simpleBox
+    orientation = 0;
 }
 
-unsigned char addBlock[8][32](unsigned char picture[8][32],unsigned char block[4][4][2]){
+void addBlock(){
+    unsigned char tempBackground[8][32];
+    memcpy(tempBackground, background, 8*32);
     int i;
     for(i=0;i<4;i++){
-        picture[block[orientation][i][0]][block[orientation][i][1]] += 1;
+        tempBackground[currentBlock[orientation][i][0]][currentBlock[orientation][i][1]] += 1;
     }
-    return picture;
+    memcpy(matrix, tempBackground, 8*32);
 }
 
-unsigned char collision(unsigned char picture[8][32]){
+unsigned char collision(){
     int i;
     int j;
     for(i=0;i<8;i++){
         for(j=0;j<32;j++){
-            if(picture[i][j]>=2)
+            if(matrix[i][j]>=2)
                 return 1;
         }
     }
     return 0;
 }
 
-unsigned char moveDown[8][32](){
+void moveDown(){
     int i;
-    unsigned char tempBlock[4][4][2] = currentBlock;
+    unsigned char tempBlock[4][4][2];
+    memcpy(tempBlock, currentBlock, 4*4*2);
     for(i=0;i<4;i++){
         if(currentBlock[orientation][i][1] == 0){
-            currentBlock = tempBlock;
+            memcpy(currentBlock, tempBlock, 4*4*2);
             newBlock();
-            return addBlock(background,currentBlock);
+            addBlock();
+            return;
         }
         else{
             int o;
@@ -91,90 +108,92 @@ unsigned char moveDown[8][32](){
             }
         }
     }
-    unsigned char newPicture[8][32] = addBlock(background,currentBlock);
-    if(collision(newPicture)){
+    addBlock();
+    if(collision()){
         for(i=0;i<4;i++){
             int o;
             for(o=0;o<4;o++){
                 currentBlock[o][i][1]++;
             }
         }
+        addBlock();
         newBlock();
-        return addBlock(background,currentBlock);
-    }
-    else{
-        return newPicture;
+        return;
     }
 }
 
-unsigned char moveLeft[8][32](){
+void moveLeft(){
     int i;
-    unsigned char tempBlock[4][4][2] = currentBlock;
+    unsigned char tempBlock[4][4][2];
+    memcpy(tempBlock, currentBlock, 4*4*2);
     for(i=0;i<4;i++){
         if(currentBlock[orientation][i][0] == 0){
-            return addBlock(background,tempBlock);
+            memcpy(currentBlock, tempBlock, 4*4*2);
+            addBlock();
+            return;
         }
         else{
             int o;
             for(o=0;o<4;o++){
-                currentBlock[o][i][0]--;
+                currentBlock[o][i][1]--;
             }
         }
     }
-    unsigned char newPicture[8][32] = addBlock(background,currentBlock);
-    if(collision(newPicture)){
+    addBlock();
+    if(collision()){
         for(i=0;i<4;i++){
             int o;
             for(o=0;o<4;o++){
-                currentBlock[o][i][0]++;
+                currentBlock[o][i][1]++;
             }
         }
-        return addBlock(background,currentBlock);
-    }
-    else{
-        return newPicture;
+        addBlock();
+        newBlock();
+        return;
     }
 }
 
-unsigned char moveRight[8][32](){
+void moveRight(){
     int i;
-    unsigned char tempBlock[4][4][2] = currentBlock;
+    unsigned char tempBlock[4][4][2];
+    memcpy(tempBlock, currentBlock, 4*4*2);
     for(i=0;i<4;i++){
         if(currentBlock[orientation][i][0] == 7){
-            return addBlock(background,tempBlock);
+            memcpy(currentBlock, tempBlock, 4*4*2);
+            addBlock();
+            return;
         }
         else{
             int o;
             for(o=0;o<4;o++){
-                currentBlock[o][i][0]++;
+                currentBlock[o][i][1]++;
             }
         }
     }
-    unsigned char newPicture[8][32] = addBlock(background,currentBlock);
-    if(collision(newPicture)){
+    addBlock();
+    if(collision()){
         for(i=0;i<4;i++){
             int o;
             for(o=0;o<4;o++){
-                currentBlock[o][i][0]--;
+                currentBlock[o][i][1]--;
             }
         }
-        return addBlock(background,currentBlock);
-    }
-    else{
-        return newPicture;
+        addBlock();
+        newBlock();
+        return;
     }
 }
 
-unsigned char putDown[8][32](){
-    unsigned char tempBlock[4][4][2] = currentBlock;
+void putDown(){
+    unsigned char tempBlock[4][4][2];
+    memcpy(tempBlock, currentBlock, 4*4*2);
     while(tempBlock[orientation][0][0]==currentBlock[orientation][0][0]){
         moveDown();
         tempBlock[orientation][0][0]--;
     }
-    return moveDown();
 }
 
-unsigned char rotate[8][32](){
+void rotate(){
     if(orientation == 3){
         orientation = 0;
     }
@@ -205,7 +224,8 @@ unsigned char rotate[8][32](){
             }
         }
     }
-    if(collision(addBlock(background,currentBlock))){
+    addBlock();
+    if(collision()){
         if(orientation == 0){
             orientation = 3;
         }
@@ -213,5 +233,10 @@ unsigned char rotate[8][32](){
             orientation--;
         }
     }
-    return addBlock(background,currentBlock);
+    addBlock();
+}
+
+int main(){
+    memcpy(currentBlock, simpleStick, 4*4*2);
+    return 0;
 }
